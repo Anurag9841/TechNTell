f77e8898c5a000e1e18811f285f9849b306b76ad
Anurag9841 <69763071+Anurag9841@users.noreply.github.com>
2021-05-24 15:07:49 +0545

    Add files via upload
    changed in cart cartcontext provider

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -2,13 +2,26 @@ import React,{useContext} from 'react'
 import {CartContext}from './CartContext'
 
 const Cart=()=>{
-    const data = useContext(CartContext);
-    console.log(data);
+    const {shoppingCart,totalPrice,qty,dispatch} = useContext(CartContext);
+    
     
     return(
         <div className="cart-container">
             <div className="cart-details" style={{marginTop:"50px"}}>
-                <h2>Shopping Cart</h2>
+                {shoppingCart.length > 0 ? 
+                shoppingCart.map(cart=>(
+                <div className="cart" key={cart.id}>
+                    <span className="cart-image"><img src= {cart.image} alt="not found"/></span>
+                    <span className="cart-product-name">{cart.name}</span>
+                    <span className="cart-product-price">Rs.{cart.price}</span>
+                    <span className="inc"><i className="fas fa-plus"></i></span>
+                    <span className="product-quantity">{cart.qty}</span>
+                    <span className="dec"><i className="fas fa-minus"></i></span>
+                    <span className="product-totalprice">100000</span>
+                    <span className="delete-product"><i className="fas fa-trash-alt"></i></span>
+                </div>
+                ))
+                :'sorry your cart is currently empty'}
             </div>
         </div>
     )

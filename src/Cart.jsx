bf3926c8e454e548d3d1f06889a2985c35448dfc
Anurag9841 <69763071+Anurag9841@users.noreply.github.com>
2021-08-02 18:24:09 +0545

    Add files via upload

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,33 +1,47 @@
-import { Button } from "reactstrap";
-import React,{useContext} from 'react'
-import {CartContext}from './CartContext'
-import { baseUrl } from './shared/baseUrl';
-const Cart=(props)=>{
-    const {shoppingCart,totalPrice,qty,dispatch} = useContext(CartContext);
-    const handleClick=(order,qty,id,price)=>{
-        order(id,qty,price);
-    }
-    
-    return(
+import React, { useContext } from 'react'
+import { CartContext } from './CartContext'
+import Khalti from "./components/login/khalti/khalti"
+
+const Cart = () => {
+    const { shoppingCart, totalPrice, qty, dispatch } = useContext(CartContext);
+
+
+    return (
         <div className="cart-container">
-            <div className="cart-details" style={{marginTop:"50px"}}>
-                {shoppingCart.length > 0 ? 
-                shoppingCart.map(cart=>(
-                <div className="cart" key={cart._id}>
-                    <span className="cart-image"><img src= {baseUrl+ cart.image} alt="not found"/></span>
-                    <span className="cart-product-name">{cart.name}</span>
-                    <span className="cart-product-price">Rs.{cart.price}</span>
-                    <span className="inc"onClick={()=>dispatch({type:"INC",id:cart.id,cart})}><i className="fas fa-plus"></i></span>
-                    <span className="product-quantity">{cart.qty}</span>
-                    <span className="dec"onClick={()=>dispatch({type:"DEC",id:cart.id,cart})}><i className="fas fa-minus"></i></span>
-                    <span className="product-totalprice">Rs.{cart.price*cart.qty}</span>
-                    <span className="delete-product" onClick={()=>dispatch({type:"DELETE",id:cart.id,cart})}><i className="fas fa-trash-alt"></i></span>
-    
-                    <Button onClick={()=>handleClick(props.order,cart.qty,cart._id,cart.price*cart.qty)}>Order</Button>
-                </div>
-                ))
-                :<h1 >sorry your cart is currently empty</h1>}
+            <div className="cart-details" style={{ marginTop: "50px" }}>
+                {shoppingCart.length > 0 ?
+                    shoppingCart.map(cart => (
+                        <div className="cart" key={cart.id}>
+                            <span className="cart-image"><img src={cart.image} alt="not found" /></span>
+                            <span className="cart-product-name">{cart.name}</span>
+                            <span className="cart-product-price">Rs.{cart.price}</span>
+                            <span className="inc" onClick={() => dispatch({ type: "INC", id: cart.id, cart })}><i className="fas fa-plus"></i></span>
+                            <span className="product-quantity">{cart.qty}</span>
+                            <span className="dec" onClick={() => dispatch({ type: "DEC", id: cart.id, cart })}><i className="fas fa-minus"></i></span>
+                            <span className="product-totalprice">Rs.{cart.price * cart.qty}</span>
+                            <span className="delete-product" onClick={() => dispatch({ type: "DELETE", id: cart.id, cart })}><i className="fas fa-trash-alt"></i></span>
+
+                        </div>
+                    ))
+                    : <h1 >sorry your cart is currently empty</h1>}
             </div>
+            {shoppingCart.length > 0 ? <div className='cart-summary'>
+                <div className="summary">
+                    <h3>Cart Summary</h3>
+                    <div className="total-items">
+                        <div className="items">Total items:</div>
+                        <div className="items-count">{qty}</div>
+                    </div>
+                    <div className="total-price-section">
+                        <div className="just-title">Total Price:</div>
+                        <div className="items-price">Rs.{totalPrice}</div>
+                    </div>
+                   
+                </div>
+                 <div className="stripe-section">
+                        <Khalti/>
+                        </div>
+            </div> : ''}
         </div>
     )
 }

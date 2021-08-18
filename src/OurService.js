4f15d2d6db257e8146f5f01888e7db4753dd3595
Anurag9841 <69763071+Anurag9841@users.noreply.github.com>
2021-08-18 18:27:25 +0545

    Add files via upload

--- src/OurService.js
+++ src/OurService.js
@@ -1,9 +1,10 @@
 import React from "react"; 
 import { NavLink } from "react-router-dom"; 
-import rujan from "./img/customise.jpg";
-import luitel from "./img/safe.jpg";
-import returns from "./img/return.jpg";
-import open from "./img/open.jpg";
+import rujan from "../images/customise.jpg";
+import luitel from "../images/safe.jpg";
+import returns from "../images/return.jpg";
+import open from "../images/open.jpg"; 
+import hi from "../images/refund.png";
 
 const OurService = () => {
     return (
@@ -13,7 +14,7 @@ const OurService = () => {
 <div className="row">
 <div className="col-md-10 col-11 mx-auto">
 <div className="mt-2 mb-4 text-center">
-    <h1 className="top_product">Our Services</h1>
+    <h1 className="service_heading"><b>Our Services</b></h1>
 </div> 
 <div className="row"> 
 {/* left side data  */}
@@ -21,32 +22,32 @@ const OurService = () => {
 <span className="badge-info badge rounded-pill px-3 py-1 my-2 font-weight-light">
 What's New 
 </span> 
-<h4>Experience the best shopping website</h4>
+<h4><b>Experience the best shopping website</b></h4>
 <h6 className="font-weight-light subtitle">We provide you the best product at cheapest price. You can 
 customise your PC according to your Budget.
 </h6> 
 {/* one more grid for services  */} 
 <div className="row mt-md-5">
 <div className="col-md-6 mt-3">
-<h6 className="font-weight-medium">Return and Refund</h6> 
+<h6 className="font-weight-medium"> <b>Return and Refund </b></h6> 
 <p>You can Return your product within 2 days of shopping</p>
 </div>
 <div className="col-md-6 mt-3">
-<h6 className="font-weight-medium">24/7 Customer Service</h6> 
+<h6 className="font-weight-medium"><b>24/7 Customer Service</b></h6> 
 <p>You can buy products at any time from Nepal</p>
 </div>
 <div className="col-md-6 mt-3">
-<h6 className="font-weight-medium">Safe Online Shopping</h6> 
+<h6 className="font-weight-medium"><b>Safe Online Shopping</b></h6> 
 <p>Experience Safe Online Shopping</p>
 </div>
 <div className="col-md-6 mt-3">
-<h6 className="font-weight-medium">Customise your PC</h6> 
+<h6 className="font-weight-medium"><b>Customise your PC</b></h6> 
 <p>Get your desired PC at Cheapest Price</p>
 </div>
 <div className="col-lg-12 my-4">
-<NavLink to="#" className="btn btn-sm btn-info">
-check more
-</NavLink>
+{/* <NavLink to="#" className="btn btn-sm btn-info">
+Shop Now
+</NavLink> */}
 </div>
 </div>
 
@@ -60,15 +61,17 @@ check more
 <div className="col-md-6">
 <div className="row">
 <div className="col-md-12 img-hover mb-4">
-<img src={returns} className="rounded img-shadow img-fluid" alt="comp1"/>
+<img src={hi} className="rounded img-shadow img-fluid" alt="comp1"/>
 </div> 
 <div className="col-md-12 img-hover mb-4">
 <img src={rujan} className="rounded img-shadow img-fluid" alt="img"/>
 </div>
 </div>
 </div> 
-{/* right side image */}
-<div className="col-md-6 uneven-box">
+
+{/* right side image */} 
+{/* uneven-box  == use after col-md-6 for uneven order of pictures   */}
+<div className="col-md-6 ">
 <div className="row">
 <div className="col-md-12 img-hover mb-4">
 <img src={luitel} className="rounded img-shadow img-fluid" alt="img"/>
